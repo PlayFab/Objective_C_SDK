2b1ab02043d63565540da492af268584193143de
PlayFab SDK Team <PlayFabSdkAccess@microsoft.com>
2022-05-09 22:24:21 +0000

    https://docs.microsoft.com/en-us/gaming/playfab/release-notes/#220509

--- PlayFabSDK/PlayFabVersion.m
+++ PlayFabSDK/PlayFabVersion.m
@@ -1,4 +1,4 @@
 #include "PlayFabVersion.h"
 
-NSString * const sdkVersion = @"0.0.220502";
-NSString * const versionString = @"ObjCSDK-0.0.220502";
+NSString * const sdkVersion = @"0.0.220509";
+NSString * const versionString = @"ObjCSDK-0.0.220509";
